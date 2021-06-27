482b290a429f2d950c773ffe97ea191e593639de
Yanjun Kang <andrewkang.ubc2020@gmail.com>
2021-06-27 16:21:45 -0700

    add header and implementation for new icon

---
+++ icons/default_still.c
@@ -0,0 +1,29 @@
+#include "logo_program.h"
+
+// This was stolen from neofetch
+volatile char* LOGO[] = {
+    "                  -`                    ",
+    "                 .o+`                   ",
+    "                `ooo/                   ",
+    "               `+oooo:                  ",
+    "              `+oooooo:                 ",
+    "              -+oooooo+:                ",
+    "            `/:-:++oooo+:               ",
+    "           `/++++/+++++++:              ",
+    "          `/++++++++++++++:             ",
+    "         `/+++ooooooooooooo/`           ",
+    "        ./ooosssso++osssssso+`          ",
+    "       .oossssso-````/ossssss+`         ",
+    "      -osssssso.      :ssssssso.        ",
+    "     :osssssss/        osssso+++.       ",
+    "    /ossssssss/        +ssssooo/-       ",
+    "  `/ossssso+/:-        -:/+osssso+-     ",
+    " `+sso+:-`                 `.-/+oso:    ",
+    "`++:.                           `-/+/   ",
+    ".`                                 `/   "
+};
+
+
+void update_logo() {
+    // do nothing, logo is a still
+}
---
+++ icons/logo_program.h
@@ -0,0 +1,10 @@
+#ifndef C_PRINT_HEADER
+#define C_PRINT_HEADER
+#include "default_still.c"
+
+// logo that will be accessed by paleofetch
+volatile char *LOGO[];
+
+void update_logo();
+
+#endif
